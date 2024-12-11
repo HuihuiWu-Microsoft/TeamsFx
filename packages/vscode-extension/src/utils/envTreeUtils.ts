5ef935b1a8bcf910a7454c2c96e076236419b5b8
Huihui Wu <huihuiwu@microsoft.com>
2024-12-11 17:36:19 +0800

    refactor: read env variables from env file instead of state json file

--- packages/vscode-extension/src/utils/envTreeUtils.ts
+++ packages/vscode-extension/src/utils/envTreeUtils.ts
@@ -1,66 +1,32 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { SubscriptionInfo } from "@microsoft/teamsfx-api";
-import { getProvisionResultJson } from "./fileSystemUtils";
 import { workspaceUri } from "../globalVariables";
 import { getV3TeamsAppId } from "./appDefinitionUtils";
-import path from "path";
-import fs from "fs-extra";
-import { dotenvUtil } from "@microsoft/teamsfx-core/build/component/utils/envUtil";
+import { envUtil } from "@microsoft/teamsfx-core/build/component/utils/envUtil";
 
-export async function getSubscriptionInfoFromEnv(
-  env: string
-): Promise<SubscriptionInfo | undefined> {
-  let provisionResult: Record<string, any> | undefined;
-
-  try {
-    provisionResult = await getProvisionResultJson(env);
-  } catch (error) {
-    // ignore error on tree view when load provision result failed.
-    return undefined;
+export async function getSubscriptionInfoFromEnv(env: string): Promise<string | undefined> {
+  const envs = await envUtil.readEnv(workspaceUri!.fsPath, env, false);
+  if (envs.isOk() && envs.value) {
+    return envs.value["AZURE_SUBSCRIPTION_ID"];
   }
-
-  if (!provisionResult) {
-    return undefined;
-  }
-
-  if (provisionResult.solution && provisionResult.solution.subscriptionId) {
-    return {
-      subscriptionName: provisionResult.solution.subscriptionName,
-      subscriptionId: provisionResult.solution.subscriptionId,
-      tenantId: provisionResult.solution.tenantId,
-    };
-  } else {
-    return undefined;
-  }
+  return undefined;
 }
 
 export async function getM365TenantFromEnv(env: string): Promise<string | undefined> {
-  const projectPath = workspaceUri!.fsPath;
-  const envFile = path.resolve(projectPath, "env", `.env.${env}`);
-  if (await fs.pathExists(envFile)) {
-    const envData = dotenvUtil.deserialize(fs.readFileSync(envFile, "utf-8"));
-    return envData.obj["TEAMS_APP_TENANT_ID"];
+  const envs = await envUtil.readEnv(workspaceUri!.fsPath, env, false);
+  if (envs.isOk() && envs.value) {
+    return envs.value["TEAMS_APP_TENANT_ID"];
   }
   return undefined;
 }
 
 export async function getResourceGroupNameFromEnv(env: string): Promise<string | undefined> {
-  let provisionResult: Record<string, any> | undefined;
-
-  try {
-    provisionResult = await getProvisionResultJson(env);
-  } catch (error) {
-    // ignore error on tree view when load provision result failed.
-    return undefined;
+  const envs = await envUtil.readEnv(workspaceUri!.fsPath, env, false);
+  if (envs.isOk() && envs.value) {
+    return envs.value["AZURE_RESOURCE_GROUP_NAME"];
   }
-
-  if (!provisionResult) {
-    return undefined;
-  }
-
-  return provisionResult.solution?.resourceGroupName;
+  return undefined;
 }
 
 export async function getProvisionSucceedFromEnv(env: string): Promise<boolean | undefined> {
--- packages/vscode-extension/src/utils/fileSystemUtils.ts
+++ packages/vscode-extension/src/utils/fileSystemUtils.ts
@@ -23,25 +23,3 @@ export async function getProjectRoot(
   const projectExists: boolean = await fs.pathExists(projectRoot);
   return projectExists ? projectRoot : undefined;
 }
-
-export async function getProvisionResultJson(
-  env: string
-): Promise<Record<string, string> | undefined> {
-  if (workspaceUri) {
-    if (!isTeamsFxProject) {
-      return undefined;
-    }
-
-    const configRoot = await getProjectRoot(workspaceUri.fsPath, `.${ConfigFolderName}`);
-
-    const provisionOutputFile = path.join(configRoot!, path.join("states", `state.${env}.json`));
-
-    if (!fs.existsSync(provisionOutputFile)) {
-      return undefined;
-    }
-
-    const provisionResult = await fs.readJSON(provisionOutputFile);
-
-    return provisionResult;
-  }
-}
