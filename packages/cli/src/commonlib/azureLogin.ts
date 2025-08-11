868f7773ad995db86c9e66216ea2119914b21e93
Huihui Wu <huihuiwu@microsoft.com>
2025-08-11 15:48:26 +0800

    feat: only enable broker for windows platform

--- packages/cli/src/commonlib/azureLogin.ts
+++ packages/cli/src/commonlib/azureLogin.ts
@@ -63,18 +63,15 @@ function getConfig(tenantId?: string): Configuration {
   } else {
     authority = "https://login.microsoftonline.com/organizations";
   }
-  const config = {
+  const config: Configuration = {
     auth: {
       clientId: "7ea7c24c-b1f6-4a20-9d11-9ae12e9e7ac0",
       authority: authority,
     },
-    broker: {
-      nativeBrokerPlugin: new NativeBrokerPlugin(),
-    },
     system: {
       loggerOptions: {
         loggerCallback(loglevel: any, message: any, containsPii: any) {
-          if (this.logLevel <= LogLevel.Error) {
+          if (this.logLevel && this.logLevel <= LogLevel.Error) {
             void CLILogProvider.log(4 - loglevel, message);
           }
         },
@@ -86,6 +83,12 @@ function getConfig(tenantId?: string): Configuration {
       cachePlugin,
     },
   };
+
+  if (featureFlagManager.getBooleanValue(FeatureFlags.BrokerAuth) && process.platform === "win32") {
+    config.broker = {
+      nativeBrokerPlugin: new NativeBrokerPlugin(),
+    };
+  }
   return config;
 }
 
--- packages/fx-core/src/common/featureFlags.ts
+++ packages/fx-core/src/common/featureFlags.ts
@@ -38,6 +38,7 @@ export class FeatureFlagName {
   static readonly SandBoxedTeam = "TEAMSFX_SANDBOXED_TEAM";
   static readonly SensitivityLabelEnabled = "TEAMSFX_SENSITIVITY_LABEL";
   static readonly DAMetaOS = "TEAMSFX_DA_METAOS";
+  static readonly BrokerAuth = "TEAMSFX_BROKER_AUTH";
 }
 
 export interface FeatureFlag {
@@ -131,6 +132,10 @@ export class FeatureFlags {
   };
   static readonly DAMetaOS = {
     name: FeatureFlagName.DAMetaOS,
+    defaultValue: "false",
+  };
+  static readonly BrokerAuth = {
+    name: FeatureFlagName.BrokerAuth,
     defaultValue: "false",
   };
 }
