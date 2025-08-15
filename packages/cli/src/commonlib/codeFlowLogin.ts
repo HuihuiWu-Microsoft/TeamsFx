ed4b6badfe436ebbbfba4a5880c929843935c114
Huihui Wu <huihuiwu@microsoft.com>
2025-08-15 17:17:43 +0800

    fix: get cached accounts from pca

--- packages/cli/src/commonlib/codeFlowLogin.ts
+++ packages/cli/src/commonlib/codeFlowLogin.ts
@@ -28,6 +28,7 @@ import {
 import { azureLoginMessage, env, m365LoginMessage } from "./common/constant";
 import CliCodeLogInstance from "./log";
 import { featureFlagManager, FeatureFlags } from "@microsoft/teamsfx-core";
+import { getAccountByHomeId } from "./common/tokenCacheUtils";
 
 export class ErrorMessage {
   static readonly loginFailureTitle = "LoginFail";
@@ -59,7 +60,6 @@ export class CodeFlowLogin {
   config: Configuration;
   port: number;
   mutex: Mutex;
-  msalTokenCache: TokenCache;
   accountName: string;
 
   constructor(scopes: string[], config: Configuration, port: number, accountName: string) {
@@ -68,21 +68,20 @@ export class CodeFlowLogin {
     this.port = port;
     this.mutex = new Mutex();
     this.pca = new PublicClientApplication(this.config);
-    this.msalTokenCache = this.pca.getTokenCache();
     this.accountName = accountName;
   }
 
   async reloadCache() {
     const accountCache = await loadAccountId(this.accountName);
     if (accountCache) {
-      const dataCache = await this.msalTokenCache.getAccountByHomeId(accountCache);
+      const dataCache = getAccountByHomeId(accountCache, await this.pca.getAllAccounts());
       if (dataCache) {
         this.account = dataCache;
       }
 
       const tenantCache = await loadTenantId(this.accountName);
       if (tenantCache) {
-        const allAccounts = await this.msalTokenCache.getAllAccounts();
+        const allAccounts = await this.pca.getAllAccounts();
         this.account = allAccounts.find((account) => account.tenantId == tenantCache);
       }
     } else {
@@ -162,7 +161,6 @@ export class CodeFlowLogin {
     for (const account of accounts) {
       await this.pca.signOut({ account: account });
     }
-    (this.msalTokenCache as any).storage.setCache({});
     await clearCache(this.accountName);
     await saveAccountId(this.accountName, undefined);
     await saveTenantId(this.accountName, undefined);
@@ -192,7 +190,7 @@ export class CodeFlowLogin {
     } else {
       let tenantedAccount: AccountInfo | undefined = undefined;
       if (tenantId) {
-        const allAccounts = await this.msalTokenCache.getAllAccounts();
+        const allAccounts = await this.pca.getAllAccounts();
         tenantedAccount = allAccounts.find((account) => account.tenantId == tenantId);
         this.account = tenantedAccount ?? this.account;
       }
